e8929b65c03e282c2fc9a0d18276a42d37686211
PigalAnastasiya <pigal.anastasiya@gmail.com>
2021-11-03 21:48:31 +0300

    feat:add solution homework8

---
+++ src/TMS-DotNet-Group-4-CashManager/Models/Inventory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMS_DotNet_Group_4_CashManager.Services;
+
+namespace TMS_DotNet_Group_4_CashManager.Models
+{
+    public class Inventory
+    {
+        public static Helper helper = new Helper();
+
+        /// <summary>
+        /// Maximum random value depending on the selected inventory.
+        /// </summary>
+
+        public IEnumerable<Product> Products { get; set; } = helper.GetRandomProducts();
+    }
+}
--- src/TMS-DotNet-Group-4-CashManager/Models/Product.cs
+++ src/TMS-DotNet-Group-4-CashManager/Models/Product.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TMS_DotNet_Group_4_CashManager.Services;
 
 namespace TMS_DotNet_Group_4_CashManager.Models
 {
